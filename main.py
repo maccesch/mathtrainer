9d42f025540bf46f963beecedebbeb99cf8c4b87
Marc-Stefan Cassola <maccesch@gmail.com>
2015-04-19 14:17:59 +0200

    checkin

---
+++ main.py
@@ -0,0 +1,97 @@
+from time import mktime
+from flask import Flask, render_template
+from datetime import date
+from sympy import S, floor, sqrt, factor, Add, Eq, symbols, latex, simplify, exp, log
+import locale
+from numpy.random import seed, random_integers as randint
+
+locale.setlocale(locale.LC_ALL, 'de_DE')
+app = Flask(__name__)
+
+
+def desolve_equation(lhs, rhs, operation_count):
+    for i in range(operation_count):
+        number = S(randint(1, 10) * (randint(0, 1) * 2 - 1))
+        op = randint(0, 10)
+        if op <= 6:
+            rhs = Add(rhs, number, evaluate=(randint(0, 1) == 1))
+            lhs = Add(lhs, number, evaluate=(randint(0, 1) == 1))
+        elif op <= 8:
+            rhs *= number
+            lhs *= number
+        else:
+            rhs /= number
+            lhs /= number
+
+    return lhs, rhs
+
+
+def generate_quadratic_equation():
+    p = S(randint(-10, 10))
+    p2 = p / 2
+
+    q = randint(-10, min(10, floor(p2 ** 2)))
+
+    x1 = simplify(-p2 + sqrt(p2 ** 2 - q))
+    x2 = simplify(-p2 - sqrt(p2 ** 2 - q))
+
+    x = symbols('x')
+    xx = x ** 2 + p * x
+
+    if randint(0, 1):
+        xx = factor(xx)
+    rhs = xx + q
+    lhs = S(0)
+
+    lhs, rhs = desolve_equation(lhs, rhs, 5)
+
+    solutions = [latex(x1)]
+    if x1 != x2:
+        solutions.append(latex(x2))
+
+    return latex(Eq(rhs, lhs)), solutions
+
+
+def generate_exponential_equation():
+    solution = S(randint(-20, 20)) + sqrt(S(randint(0, 10))) * (randint(0, 1) * 2 - 1)
+
+    x = symbols('x')
+
+    lhs = x
+    rhs = solution
+
+    lhs, rhs = desolve_equation(lhs, rhs, 2)
+
+    op = randint(0, 1)
+    if op and rhs > 0:
+        lhs = log(lhs)
+        rhs = log(rhs)
+    else:
+        lhs = exp(lhs)
+        rhs = exp(rhs)
+
+    lhs, rhs = desolve_equation(lhs, rhs, 3)
+
+    return latex(Eq(lhs, rhs)), (latex(simplify(solution)), )
+
+
+def get_equations_for_date(thedate):
+
+    seed(int(mktime(date.today().timetuple())))
+
+    equations = []
+    equations.append(generate_quadratic_equation())
+    equations.append(generate_exponential_equation())
+
+    return equations
+
+
+@app.route('/')
+def get_exercises_for_today():
+    equations = get_equations_for_date(date.today())
+
+    return render_template('exercises.html', equations=equations, date=date.today().strftime('%A, %d.%m.%Y'))
+
+
+if __name__ == '__main__':
+    app.run()
