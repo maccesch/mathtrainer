1c36420662db478ae4dc0bc8c9109b596864ccde
Marc-Stefan Cassola <maccesch@gmail.com>
2015-05-08 00:30:57 +0200

    added more complex bonus equation

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
+# coding=utf-8
 from time import mktime
 from flask import Flask, render_template
 from datetime import date
-from sympy import S, floor, sqrt, factor, Add, Eq, symbols, latex, simplify, exp, log
+from sympy import S, floor, sqrt, factor, Add, Eq, symbols, latex, simplify, exp, log, root
 import locale
-from numpy.random import seed, random_integers as randint
+from numpy.random import seed, random_integers as randint, choice
+from sympy.core.mul import Mul
+from sympy.core.numbers import Rational
 
 locale.setlocale(locale.LC_ALL, 'de_DE')
 app = Flask(__name__)
 
 
-def desolve_equation(lhs, rhs, operation_count):
+def desolve_equation(lhs, rhs, operation_count, eq_symbols=range(1, 11)):
     for i in range(operation_count):
-        number = S(randint(1, 10) * (randint(0, 1) * 2 - 1))
+        number = S(choice(eq_symbols) * (randint(0, 1) * 2 - 1))
         op = randint(0, 10)
         if op <= 6:
             rhs = Add(rhs, number, evaluate=(randint(0, 1) == 1))
             lhs = Add(lhs, number, evaluate=(randint(0, 1) == 1))
         elif op <= 8:
-            rhs *= number
-            lhs *= number
+            rhs = Mul(rhs, number, evaluate=(randint(0, 1) == 1))
+            lhs = Mul(lhs, number, evaluate=(randint(0, 1) == 1))
         else:
-            rhs /= number
-            lhs /= number
+            rhs = Mul(rhs, number**(-1), evaluate=(randint(0, 1) == 1))
+            lhs = Mul(lhs, number**(-1), evaluate=(randint(0, 1) == 1))
 
     return lhs, rhs
+
+
+def generate_hard_equation():
+    x, a, b, c = symbols('x a b c')
+
+    eq_symbols = [a, b, c] + range(-20, 0) + range(1, 21)
+
+    solution = S(choice(eq_symbols))
+
+    if (solution in (a, b, c) or solution > 0) and randint(0, 1):
+        solution = sqrt(solution)
+    if randint(0, 1):
+        solution /= choice(eq_symbols)
+
+
+    lhs = solution
+    rhs = x
+
+    lhs, rhs = desolve_equation(lhs, rhs, 10, eq_symbols)
+
+    return latex(Eq(rhs, lhs)), [latex(solution)]
 
 
 def generate_equation():
@@ -112,9 +136,17 @@ def get_equations_for_date(thedate):
     seed(int(mktime(date.today().timetuple())))
 
     equations = []
-    equations.append(generate_quadratic_equation())
-    #equations.append(generate_exponential_equation())
-    equations.append(generate_equation())
+
+    eq, sol = generate_quadratic_equation()
+    equations.append((u'Aufgabe 1', ur'Bestimme die Lösungsmenge von \(x\).', eq, sol))
+
+    # equations.append(generate_exponential_equation())
+
+    eq, sol = generate_equation()
+    equations.append((u'Aufgabe 2', ur'Löse nach \(x\) auf.', eq, sol))
+
+    eq, sol = generate_hard_equation()
+    equations.append((u'Bonushammeraufgabe', ur'Löse nach \(x\) auf. Eventuell vorkommende Symbole \(a\), \(b\) und \(c\) sind unbekannte Konstanten.', eq, sol))
 
     return equations
 
@@ -127,4 +159,5 @@ def get_exercises_for_today():
 
 
 if __name__ == '__main__':
+    # app.debug = True
     app.run()
